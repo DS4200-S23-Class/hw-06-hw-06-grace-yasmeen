738b0f885b3b0ad1733d835535a0a3e200b6ea4d
Grace Kelner <gracekelner@Graces-MacBook-Pro.local>
2023-02-26 17:35:41 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 // Javascript for hw-06
+
+
+//create frame
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 450;
+const MARGINS = {left:50, right:50, top:50, bottom:50};
+
+//scale
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+
+const FRAME1 = d3.select("#vis1")
+				  .append("svg")
+				  .attr("height", FRAME_HEIGHT)
+				  .attr("width", FRAME_WIDTH)
+				  .attr("id", "frame");
+
+const FRAME2 = d3.select("#vis2")
+				  .append("svg")
+				  .attr("height", FRAME_HEIGHT)
+				  .attr("width", FRAME_WIDTH)
+				  .attr("id", "frame");
+
+
+
+const FRAME3 = d3.select("#vis3")
+				  .append("svg")
+				  .attr("height", FRAME_HEIGHT)
+				  .attr("width", FRAME_WIDTH)
+				  .attr("id", "frame");
+
+
